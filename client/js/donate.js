921d539758b16dd3b136f60045485bd6ca1aa4e0
Jordi Bartolomé <jordi.bartolome.guillen@gmail.com>
2017-11-10 16:49:06 -0500

    New tab for donations

--- client/js/donate.js
+++ client/js/donate.js
@@ -2,6 +2,6 @@ class Donate {
 	constructor() {}
 
 	donate() {
-		window.location.href = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ZR49DGT2BWR7J";
+		window.open("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ZR49DGT2BWR7J",'_blank');
 	}
 }
